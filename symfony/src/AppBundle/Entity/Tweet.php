96daadbd7837fdcf9b885d16743add07050d885e
Mathieu Hendey <m@mathieuhendey.com>
2016-11-03 13:30:29 +0000

    Add stub entities

---
+++ symfony/src/AppBundle/Entity/Tweet.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="tweet")
+ */
+class Tweet
+{
+}
---
+++ symfony/src/AppBundle/Entity/TwitterUser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="twitter_user")
+ */
+class TwitterUser
+{
+}
