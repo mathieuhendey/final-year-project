ec15b3d513c72ab5dc4e69b12c03c810743f62ce
Mathieu Hendey <m@mathieuhendey.com>
2016-11-03 13:33:18 +0000

    Add a link to authorise with Twitter
    Just dump the user's info to the screen for now.

--- symfony/src/AppBundle/Controller/DefaultController.php
+++ symfony/src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,62 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Service\TweetFetcher;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Templating\EngineInterface;
 
+/**
+ * Class DefaultController.
+ *
+ * @Route(service="app.default_controller")
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @var TweetFetcher
      */
-    public function indexAction(Request $request)
+    private $tweetFetcher;
+
+    /**
+     * @var EngineInterface
+     */
+    private $templating;
+
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * @var Session
+     */
+    private $session;
+
+    public function __construct(
+        TweetFetcher $tweetFetcher,
+        EngineInterface $templating,
+        RouterInterface $router,
+        Session $session
+    ) {
+        $this->tweetFetcher = $tweetFetcher;
+        $this->templating = $templating;
+        $this->router = $router;
+        $this->session = $session;
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     * @Template("default/index.html.twig")
+     */
+    public function indexAction(Request $request): array
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        var_dump($this->tweetFetcher->getAccountDetails());
+
+        return [];
     }
 }
