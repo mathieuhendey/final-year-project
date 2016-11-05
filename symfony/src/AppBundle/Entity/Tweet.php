8e3e02d5a800922a1e0d4fb3474a8650363ea561
Mathieu Hendey <m@mathieuhendey.com>
2016-11-05 17:58:00 +0000

    Clean up entities

--- symfony/src/AppBundle/Entity/Tweet.php
+++ symfony/src/AppBundle/Entity/Tweet.php
@@ -1,27 +1,35 @@
 <?php
 
-/**
- * Part of the AJ02 project at Queen's University Belfast.
- *
- * PHP version 7
- *
- * @see https://gitlab.eeecs.qub.ac.uk/40100521/AJ02
- */
-
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * A generic Tweet. This could be a top-level Tweet
- * (i.e. the first Tweet in a thread of replies), or a reply to a top-level
- * Tweet.
+ * Tweet
  *
- * @author Mathieu Hendey <mhendey01@qub.ac.uk>
- *
- * @ORM\Entity
  * @ORM\Table(name="tweet")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TweetRepository")
  */
 class Tweet
 {
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
+
--- symfony/src/AppBundle/Entity/TwitterUser.php
+++ symfony/src/AppBundle/Entity/TwitterUser.php
@@ -1,30 +1,35 @@
 <?php
 
-/**
- * Part of the AJ02 project at Queen's University Belfast.
- *
- * PHP version 7
- *
- * @see https://gitlab.eeecs.qub.ac.uk/40100521/AJ02
- */
-
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
 /**
- * A Twitter user. Will store data relevant to the app's purpose, such as the
- * first time they were scanned by the app, their popularity score, as well
- * as any information from Twitter that could be useful (sign-up date, whether
- * they have a blue tick, whether their account is private etc.).
+ * TwitterUser
  *
- * This does not store any access credentials, I am still
- * deciding whether to persist credentials to the DB or just make the user
- * authorise again each time they visit the app.
- *
- * @author Mathieu Hendey <mhendey01@qub.ac.uk>
- *
- * @ORM\Entity
  * @ORM\Table(name="twitter_user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TwitterUserRepository")
  */
 class TwitterUser
 {
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
+
