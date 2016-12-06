e8664e907331b1d1a13002be1626ab1825bdd179
Mathieu Hendey <m@mathieuhendey.com>
2016-12-06 16:21:04 +0000

    Add Doctrine entity for Tweet

---
+++ symfony/src/AppBundle/Entity/Tweet.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="tweets",uniqueConstraints={@ORM\UniqueConstraint(name="id_UNIQUE", columns={"id"})})
+ */
+class Tweet
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="tweet_id", type="string", nullable=false)
+     */
+    protected $tweetId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="in_reply_to_status_id", type="string")
+     */
+    protected $inReplyToStatusId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="string")
+     */
+    protected $text;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="in_reply_to_user_id", type="string")
+     */
+    protected $inReplyToUserId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string")
+     */
+    protected $author;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author_id", type="string")
+     */
+    protected $authorId;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTweetId(): string
+    {
+        return $this->tweetId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInReplyToStatusId(): string
+    {
+        return $this->inReplyToStatusId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInReplyToUserId(): string
+    {
+        return $this->inReplyToUserId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthorId(): string
+    {
+        return $this->authorId;
+    }
+
+}
