6c8fd12ea91cf277c10dd09ffa30c9ec09f0d7ca
Mathieu Hendey <m@mathieuhendey.com>
2016-12-06 18:09:32 +0000

    Add header comment to all files

--- symfony/src/AppBundle/Controller/ResultsController.php
+++ symfony/src/AppBundle/Controller/ResultsController.php
@@ -1,10 +1,27 @@
 <?php
 
+/**
+ * Part of the AJ02 project at Queen's University Belfast.
+ *
+ * PHP version 7
+ *
+ * @see https://gitlab.eeecs.qub.ac.uk/40100521/AJ02
+ */
+
 namespace AppBundle\Controller;
 
 use AppBundle\Service\AnalysisGetter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * Handles the page showing the results of the analysis of a term.
+ *
+ * @author Mathieu Hendey <mhendey01@qub.ac.uk>
+ *
+ * @Route(service="app.results_page_controller")
+ */
 class ResultsController extends Controller
 {
     /**
--- symfony/src/AppBundle/Service/AnalysisGetter.php
+++ symfony/src/AppBundle/Service/AnalysisGetter.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Part of the AJ02 project at Queen's University Belfast.
+ *
+ * PHP version 7
+ *
+ * @see https://gitlab.eeecs.qub.ac.uk/40100521/AJ02
+ */
+
 namespace AppBundle\Service;
 
 class AnalysisGetter
