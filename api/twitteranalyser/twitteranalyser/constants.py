4b5e156617ef3a11da75f66a8fa45745977fcd79
Mathieu Hendey <m@mathieuhendey.com>
2017-02-12 13:43:58 +0000

    Change service names

--- api/twitteranalyser/twitteranalyser/constants.py
+++ api/twitteranalyser/twitteranalyser/constants.py
@@ -7,7 +7,7 @@ TWITTER_APP_SECRET = 'mLste69X6FOqNwcAw8b6tcI8m2avhnl8QYpjUXlsd5mncW6CGE'
 TWITTER_KEY = '69321956-eExmX9kaFmtutwXrrn5nfuu8p2n1bxZtXjhScUV82'
 TWITTER_SECRET = 'a832WpLH7ERGpAqhtt6K4Nj07hI83LDXaOZZxp7QhjMaM'
 
-DB_URL = 'mysql+pymysql://root:root@database/twitter_analyser?charset=utf8mb4'
+DB_URL = 'mysql+pymysql://root:root@db/twitter_analyser?charset=utf8mb4'
 TWEET_TOPIC_TABLE = 'analysis_topic'
 TWEET_USER_TABLE = 'analysis_user'
 TWEET_TABLE = 'tweet'
