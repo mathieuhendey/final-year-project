e0ef7db3c8e724158c7df9ae0b8b1a5c3e45b591
Mathieu Hendey <m@mathieuhendey.com>
2017-02-15 20:07:46 +0000

    Update travis.yml

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-ssh -t mathieu@178.62.71.43 'cd final-year-project && git pull && docker-compose up --build -d'
--- go.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-docker-compose up -d db
-docker-compose up -d
